171cfdc449e1701285fc8983ea3951a7043cf49d
Joe <maiaranagaming@gmail.com>
2023-12-31 17:23:48 -0500

    Game Module is fully set up

--- core/game/apps.py
+++ core/game/apps.py
@@ -3,4 +3,5 @@ from django.apps import AppConfig
 
 class GameConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'game'
+    name = 'core.game'
+    label = 'core_game'
