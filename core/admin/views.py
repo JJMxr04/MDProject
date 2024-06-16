1a943cdcfa518aa8d6a53bb9f55739bf89f3d620
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-06-16 19:08:30 -0400

    admin dashboard complete next will figuring out the different screen sizes

--- core/admin/views.py
+++ core/admin/views.py
@@ -17,9 +17,9 @@ def get_date_range_statistics(date_range):
         user_registrations = User.objects.filter(created__gte=start_date).annotate(day=ExtractDay('created')).values('day').annotate(count=Count('id')).order_by('day')
     elif date_range == 'monthly':
         start_date = now.replace(day=1)
-        date_format = '%b'
-        waitlist_entries = WaitlistEntry.objects.filter(created__gte=start_date).annotate(month=ExtractMonth('created')).values('month').annotate(count=Count('id')).order_by('month')
-        user_registrations = User.objects.filter(created__gte=start_date).annotate(month=ExtractMonth('created')).values('month').annotate(count=Count('id')).order_by('month')
+        date_format = '%d'
+        waitlist_entries = WaitlistEntry.objects.filter(created__gte=start_date).annotate(day=ExtractDay('created')).values('day').annotate(count=Count('id')).order_by('day')
+        user_registrations = User.objects.filter(created__gte=start_date).annotate(day=ExtractDay('created')).values('day').annotate(count=Count('id')).order_by('day')
     elif date_range == 'yearly':
         start_date = now.replace(month=1, day=1)
         date_format = '%b'
@@ -37,8 +37,8 @@ def get_date_range_statistics(date_range):
         user_registrations = User.objects.annotate(month=ExtractMonth('created')).values('month').annotate(count=Count('id')).order_by('month')
 
     if start_date:
-        waitlist_signups = {i['day' if date_range == 'last_7_days' else 'month']: i['count'] for i in waitlist_entries}
-        user_registrations = {i['day' if date_range == 'last_7_days' else 'month']: i['count'] for i in user_registrations}
+        waitlist_signups = {i['day' if date_range in ['last_7_days', 'monthly'] else 'month']: i['count'] for i in waitlist_entries}
+        user_registrations = {i['day' if date_range in ['last_7_days', 'monthly'] else 'month']: i['count'] for i in user_registrations}
     else:
         waitlist_signups = {i['month']: i['count'] for i in waitlist_entries}
         user_registrations = {i['month']: i['count'] for i in user_registrations}
@@ -58,17 +58,22 @@ def admin_dashboard(request):
         days = [(now - timezone.timedelta(days=i)).strftime(date_format) for i in range(6, -1, -1)]
         waitlist_data = [waitlist_signups.get((now - timezone.timedelta(days=i)).day, 0) for i in range(6, -1, -1)]
         registration_data = [user_registrations.get((now - timezone.timedelta(days=i)).day, 0) for i in range(6, -1, -1)]
+    elif date_range == 'monthly':
+        days_in_month = (now - timezone.timedelta(days=now.day)).day
+        days = [str(i).zfill(2) for i in range(1, days_in_month + 1)]
+        waitlist_data = [waitlist_signups.get(i, 0) for i in range(1, days_in_month + 1)]
+        registration_data = [user_registrations.get(i, 0) for i in range(1, days_in_month + 1)]
     else:
         months = [calendar.month_abbr[i] for i in range(1, 13)]
         waitlist_data = [waitlist_signups.get(i, 0) for i in range(1, 13)]
         registration_data = [user_registrations.get(i, 0) for i in range(1, 13)]
 
     context = {
-        'waitlist_signups': sum(waitlist_data if date_range == 'last_7_days' else waitlist_data),
-        'user_registrations': sum(registration_data if date_range == 'last_7_days' else registration_data),
+        'waitlist_signups': sum(waitlist_data),
+        'user_registrations': sum(registration_data),
         'date_range': date_range,
         'chart_type': chart_type,
-        'labels': days if date_range == 'last_7_days' else months,
+        'labels': days if date_range in ['last_7_days', 'monthly'] else months,
         'waitlist_data': waitlist_data,
         'registration_data': registration_data,
     }
