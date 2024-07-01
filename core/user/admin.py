02ce78123866f99d953ba5be1777344207f92b13
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-07-01 15:31:18 -0400

    fixed user activation and sign in

--- core/user/admin.py
+++ core/user/admin.py
@@ -3,20 +3,18 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['email', 'username', 'first_name', 'last_name', 'is_staff', 'is_superuser']
+    list_display = ['email', 'username', 'first_name', 'last_name', 'is_staff', 'is_admin', 'activated_link']
     search_fields = ['email', 'username', 'first_name', 'last_name']
-    list_filter = ['is_staff', 'is_superuser', 'is_active']
+    list_filter = ['is_staff', 'is_admin', 'is_active', 'activated_link']
     fieldsets = [
         ('Personal Information', {'fields': ['username', 'email', 'first_name', 'last_name', 'bio', 'avatar']}),
-        ('Permissions', {'fields': ['is_staff', 'is_superuser', 'is_active']}),
+        ('Permissions', {'fields': ['is_staff', 'is_admin', 'is_active', 'activated_link', 'is_superuser']}),
         ('Important Dates', {'fields': ['created', 'updated']}),
     ]
-    readonly_fields = ['created', 'updated']
+    readonly_fields = ['created', 'updated', 'is_superuser', 'is_active']
     ordering = ['-created']
 
     def get_queryset(self, request):
         queryset = super().get_queryset(request)
         # Customize queryset as needed, e.g., prefetch related fields
         return queryset
-
-
--- core/user/serializers.py
+++ core/user/serializers.py
@@ -15,9 +15,9 @@ class UserSerializer(AbstractSerializer):
         model = User
         # List of all the fields that can be included in a request or a response
         fields = ['id', 'username', 'first_name', 'last_name', 'bio', 'avatar', 'email', 'is_active',
-                  'created', 'updated', 'is_admin', 'is_staff']
+                  'created', 'updated', 'is_admin', 'is_staff','activated_link']
         # List of all the fields that can only be read by the user
-        read_only_field = ['is_active', 'is_admin', 'id', 'is_staff']
+        read_only_field = ['is_active', 'is_admin', 'id', 'is_staff','activated_link']
 
 class PublicUserSerializer(AbstractSerializer):
     # Rewriting some fields like the public id to be represented as the id of the object
