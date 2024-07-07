47df9c27a81572c9e5d513842b7ca29ee36e007d
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-07-07 12:44:01 -0400

    blog part1

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
     'core',
     'core.user',
     'core.auth',
+    'core.blog',
     'core.event',
     'core.game',
     'core.match',
---
+++ core/blog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from core.user.models import User
+from core.event.models.event import Event
+from django.utils import timezone
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(default=timezone.now)
+    tags = models.ManyToManyField(Tag, related_name='articles')
+    is_published = models.BooleanField(default=False)
+    image = models.ImageField(upload_to='articles/', blank=True, null=True)
+    highlight_link = models.CharField(max_length=500)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-published_at']
