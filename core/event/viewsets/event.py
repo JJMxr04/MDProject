33f12799e757a0da8a9864a632bd2d4475f3a166
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-07-19 07:57:30 -0400

    some images and viewset to allow params for searching

--- core/event/viewsets/event.py
+++ core/event/viewsets/event.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from core.event.serializers.event import EventSerializer
@@ -7,27 +8,42 @@ from core.abstract.viewsets import AbstractViewSet
 from rest_framework.response import Response
 from rest_framework.filters import SearchFilter, OrderingFilter
 
-
 class EventViewSet(AbstractViewSet):
-    http_method_names = 'get'
+    http_method_names = ['get']
     authentication_classes = (JWTAuthentication,)
     permission_classes = (IsAuthenticated,)
     filter_backends = [OrderingFilter, SearchFilter]
     serializer_class = EventSerializer
     queryset = Event.objects.all()
     ordering = ['commence_time']
-    search_fields = ['sport_title', 'away_team','home_team','commence_time','created']
-    # ordering = []
+    search_fields = ['sport_title', 'away_team', 'home_team', 'commence_time', 'created', 'sport_key']
     pagination_class = EventPagination  # Use the custom pagination class
 
     def get_queryset(self):
-        return Event.objects.get_active_events()
+        queryset = Event.objects.get_active_events()
+        allowed_params = ['sport_key', 'search','commence_time']  # Define the allowed parameters
+        filter_kwargs = {}
+
+        for param in allowed_params:
+            value = self.request.query_params.get(param, None)
+            if value:
+                if param == 'search':
+                    # Custom search logic if needed
+                    queryset = queryset.filter(
+                        Q(sport_title__icontains=value) |
+                        Q(away_team__icontains=value) |
+                        Q(home_team__icontains=value) |
+                        Q(sport_key__icontains=value)
+                    )
+                else:
+                    filter_kwargs[param] = value
+
+        queryset = queryset.filter(**filter_kwargs)
+        return queryset
 
     def get_object(self):
         obj = Event.objects.get_object_by_public_id(self.kwargs['pk'])
-
         self.check_object_permissions(self.request, obj)
-
         return obj
 
     def list(self, request, *args, **kwargs):
