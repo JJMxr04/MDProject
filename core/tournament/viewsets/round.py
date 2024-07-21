689362ce3e220faf23d3832ce7d348194d494314
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-07-21 18:13:46 -0400

    fixing some round viewset stuff

--- core/tournament/viewsets/round.py
+++ core/tournament/viewsets/round.py
@@ -2,20 +2,19 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import action
-from core.tournament.models.tournament import Round, Player, Tournament
+from core.tournament.models.tournament import Round, Player
 from core.tournament.serializers.tournament import RoundSerializer
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class RoundViewSet(viewsets.ModelViewSet):
-    authentication_classes = (JWTAuthentication,)  # Note the comma to make it a tuple
+    authentication_classes = (JWTAuthentication,)
     http_method_names = ('get', 'patch')
     serializer_class = RoundSerializer
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        user = self.request.user
-        players = Player.objects.filter(player=user)
-        return Round.objects.filter(player_1__in=players, completed=False) | Round.objects.filter(player_2__in=players, completed=False)
+        # Allow all rounds to be queried
+        return Round.objects.all()
 
     def retrieve(self, request, *args, **kwargs):
         instance = self.get_object()
@@ -23,7 +22,9 @@ class RoundViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
     def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
+        user = self.request.user
+        players = Player.objects.filter(player=user)
+        queryset = Round.objects.filter(player_1__in=players) | Round.objects.filter(player_2__in=players)
         serializer = self.get_serializer(queryset, many=True)
         return Response(serializer.data)
 
