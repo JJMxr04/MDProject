4af6c81edbe3f93122f012b6744d41ca1d4373a2
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-07-28 10:11:29 -0400

    auth false

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -158,7 +158,7 @@ AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 # AWS_QUERYSTRING_AUTH = False
-
+AWS_QUERYSTRING_AUTH = False
 # Additional optional settings
 AWS_S3_FILE_OVERWRITE = True
 AWS_DEFAULT_ACL = 'public-read'
