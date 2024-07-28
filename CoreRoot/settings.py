b190cbbe1fb6e75e8c53d049bdba1e12aeedd7c1
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-07-28 17:09:03 -0400

    changes

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -146,7 +146,10 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files (user-uploaded files)
-MEDIA_URL = f'https://{os.getenv("BUCKETEER_BUCKET_NAME")}.s3.amazonaws.com/'
+if not os.environ.get("DEBUG"):
+    MEDIA_URL = f'https://{os.getenv("BUCKETEER_BUCKET_NAME")}.s3.amazonaws.com/'
+else:
+    MEDIA_URL = f'/media/'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = os.getenv('BUCKETEER_AWS_ACCESS_KEY_ID')
