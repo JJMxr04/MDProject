9a08418353a5b92369770449b2000f72dbbbe614
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-07-28 10:56:05 -0400

    more

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -160,9 +160,9 @@ AWS_S3_OBJECT_PARAMETERS = {
 # AWS_QUERYSTRING_AUTH = False
 # AWS_QUERYSTRING_AUTH = False
 # Additional optional settings
-AWS_S3_FILE_OVERWRITE = True
+# AWS_S3_FILE_OVERWRITE = True
 AWS_DEFAULT_ACL = 'public-read'
-AWS_S3_SIGNATURE_VERSION = 's3v4'
+# AWS_S3_SIGNATURE_VERSION = 's3v4'
 
 # Configure Django REST Framework settings
 REST_FRAMEWORK = {
