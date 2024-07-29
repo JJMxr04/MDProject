45b1f6d960af63c0a3b94ce3ff41ae97073d2265
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-07-29 17:21:43 -0400

    quick update for cors origin

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -36,6 +36,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == False
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", '').split(',')
+CORS_ALLOWED_ORIGINS = os.environ.get("ALLOWED_HOSTS", '').split(',')
 
 # Application definition
 
