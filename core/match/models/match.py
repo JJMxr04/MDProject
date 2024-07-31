36d18dda710355389beb9f9339281062ead1203f
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-07-30 20:45:55 -0400

    Update match.py

--- core/match/models/match.py
+++ core/match/models/match.py
@@ -229,7 +229,7 @@ class Match(AbstractModel):
     def __str__(self):
         return f'{self.player_1} vs {self.player_2} '
 
-    def create(self, *args, **kwargs):
+    def add(self, *args, **kwargs):
         if self._state.adding:
             if not self.start_date:
                 self.start_date = timezone.now()
@@ -237,6 +237,6 @@ class Match(AbstractModel):
             if self.player_2:
                 Match.objects.accept_match(self, self.player_2)
             else:
-                super().create(*args, **kwargs)
+                super().add(*args, **kwargs)
         else:
-            super().create(*args, **kwargs)
+            super().add(*args, **kwargs)
