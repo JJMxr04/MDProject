ef3f4daa80f5217f51ceebee4304ad087d21ee92
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-08-10 00:27:10 -0400

    migrations

--- core/event/models/event.py
+++ core/event/models/event.py
@@ -71,7 +71,7 @@ class Event(AbstractModel):
     objects = EventManager()
 
     class Meta:
-        db_table = 'core.event'
+        db_table = "'core.event'"
 
     def __str__(self):
         return f"{self.home_team} Vs {self.away_team}"
