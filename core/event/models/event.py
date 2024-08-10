eb1e11276ccc240c04287efc8a50bd682c7db8c5
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-08-10 13:52:18 -0400

    Update event.py debug print statements

--- core/event/models/event.py
+++ core/event/models/event.py
@@ -11,18 +11,25 @@ from core.event.models.team import Team
 
 class EventManager(AbstractManager):
     def get_event_state(self, event_id, completed, event_scores, score1, score2):
+        print("get_event_state")
         event = self.get_object_by_id(event_id)
         if event is ObjectDoesNotExist:
+            print(1)
             return None
-        if event.completed == True:
+        if event.completed == False:
+            print(2)
             event.completed = completed
             event.scores = event_scores
-
+            print(f'score1:{score1['score']}')
+            print(f'score2:{score2['score']}')
             if score1['score'] > score2['score']:
+                print(3)
                 event.winner = score1['name']
             elif score1['score'] < score2['score']:
+                print(4)
                 event.winner = score2['name']
             else:
+                print(5)
                 event.winner = 'Tie'
 
             event.save()
