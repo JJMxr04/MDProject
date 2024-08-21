bfff0bd42fba279bd6940efbb269ccc18737108f
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-08-20 21:21:18 -0400

    Update matchUpdate.py

--- core/match/crons/matchUpdate.py
+++ core/match/crons/matchUpdate.py
@@ -7,15 +7,14 @@ from rest_framework import status
 from django.core.exceptions import ObjectDoesNotExist
 from django.utils import timezone
 from core.match.models import Match
-
 import uuid
 
 
-class MatchCron():
+class MatchCron:
 
     def completeMatches(self):
         today = timezone.now().date()
-        matches = Match.objects.filter(end_date=today, match_state='completed')
+        matches = Match.objects.filter(end_date__lte=today, match_state='completed')
 
         for match in matches:
-            Match.objects.calcutate_winner(match)
+            Match.objects.calculate_winner(match)
