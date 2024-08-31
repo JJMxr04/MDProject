91cd060db0cfb9efc32d3365724341b2284f67f1
JJMxr04 <maiaranagaming@gmail.com>
2024-08-31 00:47:08 -0400

    just some permissions for the tournament details page

--- core/portal/views/tournament/myTournamentDetail.py
+++ core/portal/views/tournament/myTournamentDetail.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from core.tournament.models import Tournament
-from core.tournament.models.tournament import Round
+from core.tournament.models.tournament import Round, Player
 from django.utils.timezone import now
 from itertools import groupby
 from operator import attrgetter
 
+from django.core.exceptions import PermissionDenied
+
 def my_tournament_detail(request, tournament_id):
     tournament = get_object_or_404(Tournament, id=tournament_id)
+
+    # Check if the user is a player in this tournament
+    if not Player.objects.filter(player=request.user.id,tournament=tournament).exists():
+        raise PermissionDenied("You are not a player in this tournament.")
     rounds = tournament.rounds.all().order_by('level_num')
 
     grouped_rounds = {level: list(rounds) for level, rounds in groupby(rounds, key=attrgetter('level_num'))}
