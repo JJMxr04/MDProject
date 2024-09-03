42bdd7d8cacaa106ec1d1226fc315b0fd8070dd0
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-09-03 18:39:51 -0400

    got the commence time working for  event list

--- core/portal/views/blog/upcoming_events_list.py
+++ core/portal/views/blog/upcoming_events_list.py
@@ -2,16 +2,25 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from core.event.models import Event, Sport  # Adjust import according to your project's structure
 from django.utils.dateparse import parse_date
-from django.core.paginator import Paginator
+from core.event.serializers.event import EventSerializer
+import json
+from uuid import UUID
+from django.core.serializers.json import DjangoJSONEncoder
+
+
+class UUIDEncoder(DjangoJSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, UUID):
+            return str(obj)
+        return super().default(obj)
 
 @login_required(login_url='/auth/login/')
 def upcoming_events_list(request):
-    # Fetch query parameters for filtering and pagination
+    # Fetch query parameters for filtering
     search_query = request.GET.get('search', '')
     selected_sport = request.GET.get('sport', '')
     start_date = request.GET.get('start_date')
     end_date = request.GET.get('end_date')
-    page = int(request.GET.get('page', 1))
 
     # Prepare filters
     filters = {}
@@ -24,18 +33,17 @@ def upcoming_events_list(request):
     if end_date:
         filters['commence_time__lte'] = parse_date(end_date)
 
-    # Fetch events with filtering and pagination
-    events_per_page = 10
+    # Fetch events with filtering
     events = Event.objects.filter(**filters).order_by('-commence_time')
-    paginator = Paginator(events, events_per_page)
-    page_obj = paginator.get_page(page)
+    
+    # Serialize events
+    
+    # Convert to JSON using custom encoder
+
 
     context = {
-        'events': page_obj.object_list,
-        'total_pages': paginator.num_pages,
-        'current_page': page_obj.number,
-        'page_range': paginator.page_range,
-        'sports': list(Sport.objects.all()),  # Assuming you have a Sport model
+        'events': events,
+        'sports': list(Sport.objects.all()),
         'search_query': search_query,
         'selected_sport': selected_sport,
         'start_date': start_date,
