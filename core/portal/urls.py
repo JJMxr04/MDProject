3b03b0d9b0d7051a1a2628cbdc0cd3180e4e2801
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-09-18 12:40:37 -0400

    finally got the events URLS to work

--- core/portal/urls.py
+++ core/portal/urls.py
@@ -1,15 +1,16 @@
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 from .views import UserProfileUpdateView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from core.event.urls import urlpatterns as eventUrls
 
 app_name = 'core-portal'
 
 urlpatterns = [
     path('dashboard/', views.portal_dashboard, name='portal-dashboard'),
-    path('event/', include(('core.event.urls', 'core-event'), namespace='core-event')),  # Ensure this line is correct
+    path('event/', include(eventUrls)), # Ensure this line is correct
     path('my/tournaments/', views.my_tournaments, name='portal-my-tournaments'),
     path('my/tournaments/<uuid:tournament_id>/', views.my_tournament_detail, name='portal-my-tournament-detail'),
     path('tournament/round/<uuid:round_id>/', views.my_round_detail_view, name='portal-my-tournament-round-detail'),
