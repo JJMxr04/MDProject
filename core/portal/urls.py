73ab4e65abbd9a1134220f1e7aa7a91bd1a021b5
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-09-18 15:07:57 -0400

    fixed matches

--- core/portal/urls.py
+++ core/portal/urls.py
@@ -5,19 +5,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from core.event.urls import urlpatterns as eventUrls
+from core.match.urls import urlpatterns as matchUrls
 
 app_name = 'core-portal'
 
 urlpatterns = [
     path('dashboard/', views.portal_dashboard, name='portal-dashboard'),
     path('event/', include(eventUrls)), # Ensure this line is correct
+    path('match/', include(matchUrls)),
     path('my/tournaments/', views.my_tournaments, name='portal-my-tournaments'),
     path('my/tournaments/<uuid:tournament_id>/', views.my_tournament_detail, name='portal-my-tournament-detail'),
     path('tournament/round/<uuid:round_id>/', views.my_round_detail_view, name='portal-my-tournament-round-detail'),
     path('profile/', UserProfileUpdateView.as_view(), name='profile'),
-    path('match/<uuid:match_id>/', views.my_match_detail_view, name='portal-my-match-detail'),
-    path('match/<uuid:match_id>/upload_pick/', views.upload_pick, name='portal-upload_pick'),
-    path('match/event/<uuid:event_id>/outcomes/', views.event_outcomes, name='portal-match-event-outcomes'),
-    path('match/game/<uuid:game_id>/market/', views.event_markets, name='portal-match-event-market'),
-    path('match/game/<uuid:game_id>/player_2_select_outcome/', views.player_2_select_outcome, name='portal-match-game-player_2_select_outcome'),
 ]
--- core/portal/views/__init__.py
+++ core/portal/views/__init__.py
@@ -1,6 +1,5 @@
 from  .dashboard import portal_dashboard
-from .blog import upcoming_events_list , upcoming_event_detail
+
 from .user import UserProfileUpdateView
 from .tournament import my_tournaments, my_tournament_detail
 from .round import  my_round_detail_view
-from .match import my_match_detail_view, upload_pick, event_outcomes, event_markets, player_2_select_outcome
