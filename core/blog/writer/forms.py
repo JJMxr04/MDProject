3c561358f5cb09948992ff5c76f26e47635289b4
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-10-03 16:41:58 -0400

    updating the article is done, just need to make some minor stuff top it

--- core/blog/writer/forms.py
+++ core/blog/writer/forms.py
@@ -1,9 +1,21 @@
 from .models import Article, Tag
-from django.forms import ModelForm
+from django.forms import ModelForm, TextInput
 
 class ArticleForm(ModelForm):
 
     class Meta:
         model = Article
         fields = ['title',   'content', 'tags','is_premium',]
-        
+
+
+class UpdateArticleForm(ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'event', 'outcome', 'content', 'tags', 'is_premium']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Make 'event' and 'outcome' fields disabled (read-only)
+        self.fields['event'].disabled = True
+        self.fields['outcome'].disabled = True
+
--- core/blog/writer/views/articleViews.py
+++ core/blog/writer/views/articleViews.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from core.blog.writer.decorator import writer_required
+from core.blog.writer.decorator import writer_required, author_required
 from core.event.models import Event, Outcome
 from core.blog.writer.serializers.article import ArticleSerializer
-from core.blog.writer.forms import ArticleForm
+from core.blog.writer.forms import ArticleForm, UpdateArticleForm
 from core.blog.writer.models import Article
 from django.http import HttpResponse, HttpResponseBadRequest
 
@@ -19,5 +19,16 @@ def my_articles(request):
 
 @login_required(login_url='/auth/login/')
 @writer_required
-def update_article(request,art_id):
-    pass
+@author_required
+def update_article(request, article_id):  # Ensure article_id is included
+    article = Article.objects.get(id=article_id)
+    form = UpdateArticleForm(instance=article)
+    if request.method =='POST':
+        form = UpdateArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            return redirect('core-portal:writer-my-articles')
+   
+    context = {'UpdateArticleForm':form}
+
+    return render(request,'portal/blog/writer/update-article.html', context)
