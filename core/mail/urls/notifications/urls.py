53d2d5baf363a5f38fd2a4915eda348fff1ddc37
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-10-16 19:29:39 -0400

    inivtes are going well

--- core/mail/urls/notifications/urls.py
+++ core/mail/urls/notifications/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('notifications/',views.get_notifications, name='get-notifications'),
     path('notifications/<uuid:not_id>/', views.read_notifications, name='read_notifications'),
     path('create-invite/', views.create_invite, name='create_invite'),
-    path('invite-success/', views.create_invite, name='invite-success'),
+    path('invite-success/', views.success_invite, name='invite-success'),
+    path('invite-accept/<uuid:invite_id>', views.accept_invite, name='invite-accept'),
 ]
