3be75102a258993b5ba8ef7f3b77d64095da6dad
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-10-17 16:03:00 -0400

    more debugging

--- core/event/views/upcoming_event_detail.py
+++ core/event/views/upcoming_event_detail.py
@@ -8,23 +8,31 @@ import json
 
 @login_required(login_url='/auth/login/')
 def upcoming_event_detail(request, event_id):
+    print('event1')
     # Attempt to retrieve the event or log an error if not found
     try:
+        print('event2')
         event = get_object_or_404(Event, pk=event_id)
     except Event.DoesNotExist:
+        print('event3')
         logger.error(f"Event with ID {event_id} does not exist.")
         return HttpResponseNotFound("Event not found")
-    
+    print('event4')
     # Serialize the event data for the template
     try:
+        print('event5')
         event_data = EventBookmakerSerializer(event).data
     except Exception as e:
+        print('event6')
         logger.error(f"Error serializing event data: {str(e)}")
         return HttpResponseNotFound("Error processing event data")
 
     # Add the event data to the context for rendering
-    context = {
-        'event': event_data,
+    # context = {
+    #     'event': event_data,
+    # }
+        context = {
+        'event': '{}',
     }
 
     return render(request, 'portal/event/upcoming_event_detail.html', context)
