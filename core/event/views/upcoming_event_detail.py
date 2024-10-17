c16a181daf38f3e8fbf19659cf0fea14d89d0840
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-10-17 15:18:42 -0400

    event prints

--- core/event/views/upcoming_event_detail.py
+++ core/event/views/upcoming_event_detail.py
@@ -8,12 +8,14 @@ import json
 
 @login_required(login_url='/auth/login/')
 def upcoming_event_detail(request, event_id):
+    print('event1')
     event = get_object_or_404(Event, pk=event_id)
-
+    print('event2')
     # Add necessary processing logic for bookmakers, markets, and outcomes
     # Assuming event.bookmakers is a preprocessed list of dictionaries with relevant data
     context = {
         'event': EventBookmakerSerializer(event).data,
     }
-
+    
+    print('event3')
     return render(request, 'portal/event/upcoming_event_detail.html', context)
