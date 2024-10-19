06c0a6d7b77588b81b2c8bf8a0b55cd1bb7870f6
JJMxr04 <maiaranagaming@gmail.com>
2024-10-19 11:48:34 -0400

    writer navbar remove 0nbosarding

--- core/blog/writer/views/writerDashboard.py
+++ core/blog/writer/views/writerDashboard.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from core.blog.writer.decorator import writer_required
 from core.event.models import Event, Sport  # Adjust import according to your project's structure
@@ -7,10 +7,37 @@ from core.event.serializers.event import EventSerializer
 import json
 from uuid import UUID
 from django.core.serializers.json import DjangoJSONEncoder
+import stripe
+import os
+import json
+
+stripe.api_key = settings.STRIPE_API_KEY
+stripe.api_version = '2023-10-16'
 
 
 @login_required(login_url='/auth/login/')
 @writer_required
 def writer_dashboard(request):
+    user = request.user
 
-    return render(request,'portal/blog/writer/writer-dashboard.html')
+    # Assuming the user model has a field for Stripe account ID
+    stripe_account_id = user.stripe_account_id
+
+    if not stripe_account_id:
+        # No connected account, redirect to onboarding
+        return redirect('core-portal:onboarding')  # Replace 'onboarding_page' with your actual onboarding URL name
+
+    # Fetch the account details from Stripe
+    try:
+        account = stripe.Account.retrieve(stripe_account_id)
+    except stripe.error.StripeError:
+        # Handle any potential error here (e.g., log the error)
+        return redirect('core-portal:onboarding') 
+
+    # Check if the account is fully set up
+    if account['charges_enabled'] and account['details_submitted']:
+        # Account is fully set up, display the dashboard
+         return render(request,'portal/blog/writer/writer-dashboard.html')
+    else:
+        # Account is not fully set up, redirect to onboarding
+        return redirect('core-portal:onboarding') 
