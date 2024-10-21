81360b430510a2865776bca15164cf577cb238aa
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-10-21 17:14:16 -0400

    is subbed or not subbed pages

--- core/blog/client/views/writerSubscribe.py
+++ core/blog/client/views/writerSubscribe.py
@@ -1,19 +1,31 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
-from core.blog.writer.models import SubscriptionPlan
+from core.blog.writer.models import SubscriptionPlan, Article
 from core.user.models import User
 from core.user.serializers import WriterSerializer
 from core.blog.writer.serializers.subscriptionPlan import SubscriptionPlanSerializer
+from core.blog.client.models import Subscription  # Adjust import according to your project's structure
 import stripe
-
-
 
 @login_required(login_url='/auth/login/')
 def writer_subscribe(request, writer_id):
     writer = User.objects.filter(public_id=writer_id).first()
     existing_plan = SubscriptionPlan.objects.filter(writer=writer).first()
 
+    # Check if the user is subscribed to the writer
+    is_subscribed = Subscription.objects.active_subscriptions(user=request.user).filter(writer=writer).exists()
+
+    if is_subscribed:
+        # If the user is subscribed, display the articles from the writer
+        articles = Article.objects.filter(author=writer).order_by('-date_published')
+        context = {
+            'articles': articles,
+            'writer': writer,
+        }
+        return render(request, 'portal/blog/client/client-writer-is-subscribed.html', context)
+
+    # If the user is not subscribed, show the subscription page
     writer_ser = WriterSerializer(writer).data
     existing_plan_ser = SubscriptionPlanSerializer(existing_plan).data
 
