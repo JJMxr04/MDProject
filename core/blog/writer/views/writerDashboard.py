a6f641125d2cc17b03f0b493cfa7a33f3d8ae14a
JJMxr04 <maiaranagaming@gmail.com>
2024-10-20 22:33:06 -0400

    fix for len

--- core/blog/writer/views/writerDashboard.py
+++ core/blog/writer/views/writerDashboard.py
@@ -45,7 +45,7 @@ def writer_dashboard(request):
             pending_balance = balance['pending'][0]['amount'] / 100  # Convert from cents to dollars
 
 
-            active_subcriptions = len(Subscription.objects.writer_active_subscriptions(writer=request.user))
+            active_subcriptions = Subscription.objects.writer_active_subscriptions(writer=request.user)
             current_subscription_price= SubscriptionPlan.objects.filter(wrier=request.user).price,
             # Fetch the last payout
             payouts = stripe.Payout.list(stripe_account=stripe_account_id)
