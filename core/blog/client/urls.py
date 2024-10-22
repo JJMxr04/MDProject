13d02e5c9e1fcad813744337b0e64f5d7d1d7a79
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-10-22 19:58:54 -0400

    fix an internal error about a bad bracket in clients.urls.py

--- core/blog/client/urls.py
+++ core/blog/client/urls.py
@@ -10,6 +10,5 @@ urlpatterns = [
     path('writer', views.writer_list, name='client-writer-list'),
     path('writer/<str:writer_id>', views.writer_subscribe, name='client-writer-subscribe'),
     path('writers/toggle-subscription/', views.toggle_subscription, name='client-writer-toggle-subscription'),
-]
 
 ]
