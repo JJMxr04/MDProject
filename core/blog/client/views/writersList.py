91910fbb794842dffb93b2e6dccf2d87a8bafdb1
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-10-22 17:27:42 -0400

    adding search functionality for writers

--- core/blog/client/views/writersList.py
+++ core/blog/client/views/writersList.py
@@ -1,17 +1,24 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from core.blog.writer.models import Article
-from core.blog.client.models import Subscription  # Adjust import according to your project's structure
 from core.user.models import User
 from core.user.serializers import WriterSerializer
 
 @login_required(login_url='/auth/login/')
 def writer_list(request):
+    # Get the search query from the request's GET parameters
+    query = request.GET.get('search', '')
 
-    writers = User.objects.filter(is_writer=True)
-    writers_ser = WriterSerializer(writers, many=True).data 
-    # Get all active subscriptions of the current user
-    context={'writers':writers_ser}
-    print(writers_ser)
+    # Filter writers by username if a search query is present
+    if query:
+        writers = User.objects.filter(is_writer=True, username__icontains=query)
+    else:
+        writers = User.objects.filter(is_writer=True)
     
-    return render(request, 'portal/blog/client/client-writer-list.html',context)
+    writers_ser = WriterSerializer(writers, many=True).data 
+    
+    context = {
+        'writers': writers_ser,
+        'search_query': query,  # Pass the search query back to the template
+    }
+    
+    return render(request, 'portal/blog/client/client-writer-list.html', context)
