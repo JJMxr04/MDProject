104fff346a53b9b5345abd40499b75ea5a2742e3
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-10-22 16:01:27 -0400

    fixing detail view

--- core/payments/views.py
+++ core/payments/views.py
@@ -461,6 +461,9 @@ def invoice_detail(request, invoice_id):
         try:
             # Retrieve the invoice from Stripe
             invoice = stripe.Invoice.retrieve(invoice_id)
+            # Perform division of amount_due here
+            if invoice.amount_due is not None:
+                invoice.amount_due = invoice.amount_due / 100
         except stripe.error.StripeError as e:
             print(f"Error fetching invoice: {e}")
 
