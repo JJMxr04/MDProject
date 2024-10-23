c38141bb2c3c80a1d88a37b6480d99ea80c8881e
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-10-23 17:53:35 -0400

    Update urls.py

--- core/blog/client/urls.py
+++ core/blog/client/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('client-dashboard/', views.client_dashboard, name='client-dashboard'),
     path('client-feed/', views.client_feed, name='client-feed'),
     path('writer', views.writer_list, name='client-writer-list'),
-    path('writer/<str:writer_id>', views.writer_subscribe, name='client-writer-subscribe'),
+    path('writer/<uuid:writer_id>', views.writer_subscribe, name='client-writer-subscribe'),
     path('writers/toggle-subscription/', views.toggle_subscription, name='client-writer-toggle-subscription'),
 
 ]
