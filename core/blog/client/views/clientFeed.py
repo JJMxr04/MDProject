678aafd07dbdb594125797068f31667643b191c2
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-10-24 16:59:39 -0400

    infinite scroll 1

--- core/blog/client/views/clientFeed.py
+++ core/blog/client/views/clientFeed.py
@@ -12,15 +12,32 @@ def client_feed(request):
     
     articles = Article.objects.filter(author__in=subscribed_writers, is_published=True).order_by('-date_published')
     
-    paginator = Paginator(articles, 10)
+    paginator = Paginator(articles, 10)  # 10 articles per page
     page_number = request.GET.get('page', 1)
     page_obj = paginator.get_page(page_number)
 
     if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-        article_list_html = render(request, 'portal/blog/client/partials/article_list.html', {'articles': page_obj}).content.decode('utf-8')
-        return JsonResponse({'html': article_list_html, 'has_next': page_obj.has_next()})
+        articles_data = [
+            {
+                'title': article.title,
+                'writer': {
+                    'username': article.author.username,
+                    'avatar': article.author.avatar.url if article.author.avatar else '/static/images/default-avatar.png'
+                },
+                'date_published': article.date_published.strftime('%Y-%m-%d'),
+                'content': article.content[:200] + '...',  # Show a snippet
+                'event': article.event.name if article.event else 'N/A',
+                'market_key': article.outcome.market.key if article.outcome else 'N/A',
+                'outcome_name': article.outcome.name if article.outcome else 'N/A',
+                'outcome_price': article.outcome.price if article.outcome else 'N/A',
+                'outcome_point': article.outcome.point if article.outcome else 'N/A',
+            } for article in page_obj
+        ]
 
-    context = {
-        'articles': page_obj,
-    }
+        return JsonResponse({
+            'articles': articles_data,
+            'has_next': page_obj.has_next(),
+        })
+
+    context = {'articles': page_obj}
     return render(request, 'portal/blog/client/client-feed.html', context)
