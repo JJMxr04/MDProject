20257135cc4e6b0ef0e81f7ffd991ac1ca70f212
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-10-24 17:35:45 -0400

    client feed back to square 1 again

--- core/blog/client/views/clientFeed.py
+++ core/blog/client/views/clientFeed.py
@@ -1,50 +1,22 @@
-from django.core.paginator import Paginator
-from django.http import JsonResponse
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from core.blog.writer.models import Article
-from core.blog.client.models import Subscription
+from core.blog.client.models import Subscription  # Adjust import according to your project's structure
 
 @login_required(login_url='/auth/login/')
 def client_feed(request):
     # Get all active subscriptions of the current user
     subscriptions = Subscription.objects.active_subscriptions(user=request.user)
+    
+    # Extract all the writers (authors) the user is subscribed to
     subscribed_writers = subscriptions.values_list('writer', flat=True)
-
-    # Handle pagination
-    page = request.GET.get('page', 1)  # Get current page number from the request
-    articles_query = Article.objects.filter(author__in=subscribed_writers, is_published=True).order_by('-date_published')
-    paginator = Paginator(articles_query, 10)  # Paginate by 10 articles per page
-
-    if request.is_ajax():
-        articles_page = paginator.get_page(page)
-        articles_data = [
-            {
-                'title': article.title,
-                'writer': {
-                    'username': article.writer.username,
-                    'avatar_url': article.writer.avatar.url
-                },
-                'date_created': article.date_created.strftime('%Y-%m-%d'),
-                'event': article.event,
-                'market': article.outcome.market.key,
-                'outcome': {
-                    'name': article.outcome.name,
-                    'price': article.outcome.price,
-                    'point': article.outcome.point,
-                },
-                'content': article.content
-            }
-            for article in articles_page
-        ]
-
-        return JsonResponse({
-            'articles': articles_data,
-            'has_next': articles_page.has_next()
-        })
-
-    # Initial page load
+    
+    # Get all articles from those writers, ordered by date_published
+    articles = Article.objects.filter(author__in=subscribed_writers,is_published=True).order_by('-date_published')
+    
+    # Pass the articles to the template context
     context = {
-        'articles': paginator.get_page(1)  # Load the first page of articles
+        'articles': articles
     }
+
     return render(request, 'portal/blog/client/client-feed.html', context)
