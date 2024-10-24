ce4873a2ed9b750dcd0fb9a0952d42448d43ba8d
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-10-24 17:25:21 -0400

    client feed back to square 1

--- core/blog/client/views/clientFeed.py
+++ core/blog/client/views/clientFeed.py
@@ -1,46 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.core.paginator import Paginator
-from django.http import JsonResponse
 from core.blog.writer.models import Article
-from core.blog.client.models import Subscription
+from core.blog.client.models import Subscription  # Adjust import according to your project's structure
 
 @login_required(login_url='/auth/login/')
 def client_feed(request):
+    # Get all active subscriptions of the current user
     subscriptions = Subscription.objects.active_subscriptions(user=request.user)
+    
+    # Extract all the writers (authors) the user is subscribed to
     subscribed_writers = subscriptions.values_list('writer', flat=True)
     
-    articles = Article.objects.filter(author__in=subscribed_writers, is_published=True).order_by('-date_published')
+    # Get all articles from those writers, ordered by date_published
+    articles = Article.objects.filter(author__in=subscribed_writers,is_published=True).order_by('-date_published')
     
-    paginator = Paginator(articles, 10)  # 10 articles per page
-    page_number = request.GET.get('page', 1)
-    page_obj = paginator.get_page(page_number)
-
-    articles_data = [
-        {
-            'title': article.title,
-            'writer': {
-                'username': article.author.username,
-                'avatar': article.author.avatar.url if article.author.avatar else '/static/images/default-avatar.png'
-            },
-            'date_published': article.date_published.strftime('%Y-%m-%d'),
-            'content': article.content[:200] + '...',  # Show a snippet
-            'event': article.event if article.event else 'N/A',
-            'market_key': article.outcome.market.key if article.outcome else 'N/A',
-            'outcome_name': article.outcome.name if article.outcome else 'N/A',
-            'outcome_price': article.outcome.price if article.outcome else 'N/A',
-            'outcome_point': article.outcome.point if article.outcome else 'N/A',
-        } for article in page_obj
-    ]
-
-    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-        return JsonResponse({
-            'articles': articles_data,
-            'has_next': page_obj.has_next(),
-        })
-
+    # Pass the articles to the template context
     context = {
-        'articles_data': articles_data,  # Pass initial articles data
-        'has_next': page_obj.has_next(),
+        'articles': articles
     }
+
     return render(request, 'portal/blog/client/client-feed.html', context)
