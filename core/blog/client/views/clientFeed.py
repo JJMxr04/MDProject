1dd2e662ee1f50d7effd1dff1453cdc9e841a46d
JJMxr04 <maiaranagaming@gmail.com>
2024-10-24 23:41:33 -0400

    Update clientFeed.py

--- core/blog/client/views/clientFeed.py
+++ core/blog/client/views/clientFeed.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from core.blog.writer.models import Article
 from core.blog.client.models import Subscription
+from django.core.paginator import Paginator 
 
 @login_required(login_url='/auth/login/')
 def client_feed(request):
