0ddf101a2a0f710175c6dccf96544e397e1ae1eb
JJMxr04 <maiaranagaming@gmail.com>
2024-10-24 23:33:00 -0400

    update stuff

--- core/blog/client/urls.py
+++ core/blog/client/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'core-blog-client'
 
 urlpatterns = [
     path('client-dashboard/', views.client_dashboard, name='client-dashboard'),
     path('client-feed/', views.client_feed, name='client-feed'),
-    path('writer', views.writer_list, name='client-writer-list'),
-    path('writer/<str:writer_id>', views.writer_subscribe, name='client-writer-subscribe'),
+    path('writer/', views.writer_list, name='client-writer-list'),
+    path('writer/<str:writer_id>/', views.writer_subscribe, name='client-writer-subscribe'),
     path('writers/toggle-subscription/', views.toggle_subscription, name='client-writer-toggle-subscription'),
-
 ]
--- core/blog/client/views/clientFeed.py
+++ core/blog/client/views/clientFeed.py
@@ -1,7 +1,8 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from core.blog.writer.models import Article
-from core.blog.client.models import Subscription  # Adjust import according to your project's structure
+from core.blog.client.models import Subscription
 
 @login_required(login_url='/auth/login/')
 def client_feed(request):
@@ -12,11 +13,34 @@ def client_feed(request):
     subscribed_writers = subscriptions.values_list('writer', flat=True)
     
     # Get all articles from those writers, ordered by date_published
-    articles = Article.objects.filter(author__in=subscribed_writers,is_published=True).order_by('-date_published')
+    articles = Article.objects.filter(
+        author__in=subscribed_writers,
+        is_published=True
+    ).order_by('-date_published')
     
-    # Pass the articles to the template context
+    # Paginate the articles, 5 articles per page
+    page = request.GET.get('page', 1)
+    paginator = Paginator(articles, 5)
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
+    
+    # Determine if there are more articles to load
+    is_paginated = articles.has_next()
+
+    # Pass the articles and pagination info to the template context
     context = {
-        'articles': articles
+        'articles': articles,
+        'is_paginated': is_paginated,
     }
 
+    # Check if the request is an AJAX request
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        # If the request is an AJAX request, render only the articles template
+        return render(request, 'portal/blog/client/partials/article_list.html', context)
+
+    # For non-AJAX requests, render the full template
     return render(request, 'portal/blog/client/client-feed.html', context)
