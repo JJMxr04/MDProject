fe00fb6adc395cff53f2240397e02f2ed77bf876
JJMxr04 <maiaranagaming@gmail.com>
2024-10-24 22:22:09 -0400

    clientfeedview to the urls

--- core/blog/client/views/__init__.py
+++ core/blog/client/views/__init__.py
@@ -1,4 +1,4 @@
 from .clientDashboard import client_dashboard
-from .clientFeed import client_feed
+from .clientFeed import ClientFeedView
 from .writersList import writer_list, toggle_subscription
 from .writerSubscribe import writer_subscribe
