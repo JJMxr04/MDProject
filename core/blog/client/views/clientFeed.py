8d72c7afb672af698fddc3d0ed38f459b913d67f
JJMxr04 <maiaranagaming@gmail.com>
2024-10-24 23:51:22 -0400

    back to square 1

--- core/blog/client/views/clientFeed.py
+++ core/blog/client/views/clientFeed.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from core.blog.writer.models import Article
-from core.blog.client.models import Subscription
-from django.core.paginator import Paginator 
+from core.blog.client.models import Subscription  # Adjust import according to your project's structure
 
 @login_required(login_url='/auth/login/')
 def client_feed(request):
+    # Get all active subscriptions of the current user
     subscriptions = Subscription.objects.active_subscriptions(user=request.user)
+    
+    # Extract all the writers (authors) the user is subscribed to
     subscribed_writers = subscriptions.values_list('writer', flat=True)
-    articles = Article.objects.filter(author__in=subscribed_writers, is_published=True).order_by('-date_published')
-
-    paginator = Paginator(articles, 5)  # Display 5 articles per page
-    page_number = request.GET.get('page')
-    articles_page = paginator.get_page(page_number)
-
+    
+    # Get all articles from those writers, ordered by date_published
+    articles = Article.objects.filter(author__in=subscribed_writers,is_published=True).order_by('-date_published')
+    
+    # Pass the articles to the template context
     context = {
-        'articles': articles_page,
-        'is_paginated': articles_page.has_other_pages(),
+        'articles': articles
     }
 
     return render(request, 'portal/blog/client/client-feed.html', context)
