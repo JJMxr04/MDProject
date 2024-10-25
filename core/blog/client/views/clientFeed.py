34edfca8e12b5ab2b4ee55feea45d558567b1f74
JJMxr04 <maiaranagaming@gmail.com>
2024-10-25 09:40:58 -0400

    hopefully

--- core/blog/client/views/clientFeed.py
+++ core/blog/client/views/clientFeed.py
@@ -1,7 +1,8 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from core.blog.writer.models import Article
-from core.blog.client.models import Subscription  # Adjust import according to your project's structure
+from core.blog.client.models import Subscription
 
 @login_required(login_url='/auth/login/')
 def client_feed(request):
@@ -12,11 +13,22 @@ def client_feed(request):
     subscribed_writers = subscriptions.values_list('writer', flat=True)
     
     # Get all articles from those writers, ordered by date_published
-    articles = Article.objects.filter(author__in=subscribed_writers,is_published=True).order_by('-date_published')
+    articles_list = Article.objects.filter(author__in=subscribed_writers, is_published=True).order_by('-date_published')
     
-    # Pass the articles to the template context
+    # Set up pagination
+    page = request.GET.get('page', 1)
+    paginator = Paginator(articles_list, 5)  # Display 5 articles per page
+    try:
+        articles = paginator.page(page)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
+    
+    # Pass the paginated articles to the template context
     context = {
-        'articles': articles
+        'articles': articles,
+        'is_paginated': articles.has_other_pages()  # To show the "Load More" button
     }
 
     return render(request, 'portal/blog/client/client-feed.html', context)
