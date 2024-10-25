5512d35eae1c28a08811ba9d29f297d9098112ad
JJMxr04 <maiaranagaming@gmail.com>
2024-10-24 23:35:48 -0400

    fuck

--- core/blog/client/views/clientFeed.py
+++ core/blog/client/views/clientFeed.py
@@ -1,8 +1,7 @@
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from core.blog.writer.models import Article
-from core.blog.client.models import Subscription
+from core.blog.client.models import Subscription  # Adjust import according to your project's structure
 
 @login_required(login_url='/auth/login/')
 def client_feed(request):
@@ -13,34 +12,11 @@ def client_feed(request):
     subscribed_writers = subscriptions.values_list('writer', flat=True)
     
     # Get all articles from those writers, ordered by date_published
-    articles = Article.objects.filter(
-        author__in=subscribed_writers,
-        is_published=True
-    ).order_by('-date_published')
+    articles = Article.objects.filter(author__in=subscribed_writers,is_published=True).order_by('-date_published')
     
-    # Paginate the articles, 5 articles per page
-    page = request.GET.get('page', 1)
-    paginator = Paginator(articles, 5)
-    try:
-        articles = paginator.page(page)
-    except PageNotAnInteger:
-        articles = paginator.page(1)
-    except EmptyPage:
-        articles = paginator.page(paginator.num_pages)
-    
-    # Determine if there are more articles to load
-    is_paginated = articles.has_next()
-
-    # Pass the articles and pagination info to the template context
+    # Pass the articles to the template context
     context = {
-        'articles': articles,
-        'is_paginated': is_paginated,
+        'articles': articles
     }
 
-    # Check if the request is an AJAX request
-    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-        # If the request is an AJAX request, render only the articles template
-        return render(request, 'portal/blog/client/partials/article_list.html', context)
-
-    # For non-AJAX requests, render the full template
     return render(request, 'portal/blog/client/client-feed.html', context)
