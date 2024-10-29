9f964f3a8f05a3a19fb6d24871b0fd10de66bfdc
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-10-29 16:44:58 -0400

    Update views.py

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 def robots_txt(request):
     lines = [
-        "User-agent: *",
-        "Disallow: /",
+        "User-agent: Googlebot",
+        "Disallow: ",
     ]
     return HttpResponse("\n".join(lines), content_type="text/plain")
