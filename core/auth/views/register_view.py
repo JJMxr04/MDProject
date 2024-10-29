7afb845dba853cadb1a5f1a7ebb87fd7182564f1
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-10-29 17:26:01 -0400

    Update register_view.py

--- core/auth/views/register_view.py
+++ core/auth/views/register_view.py
@@ -15,13 +15,15 @@ class RegisterView(View):
     def post(self, request):
         form = self.form_class(request.POST)
         if form.is_valid():
-            user = form.save()
+            user = form.save(commit=False)
+            user.save()  # Save the user object first
+            form.save_m2m()  # If your form has ManyToMany fields
+
+            # Pass `request` to `send_activation_email`
+            send_activation_email(user, request)  # Pass request here
             login(request, user)
             messages.success(request, 'Registration successful! You are now logged in.')
-            return redirect('core-portal:portal-dashboard')  # Replace 'home' with the name of your home page URL pattern
+            return redirect('home')
         else:
-            messages.error(request, 'Registration failed. Please correct the errors below.')
-            for field, errors in form.errors.items():
-                for error in errors:
-                    messages.warning(request, f"{field.capitalize()}: {error}")
+            messages.error(request, 'Registration failed. Please check the form for errors.')
         return render(request, self.template_name, {'form': form})
