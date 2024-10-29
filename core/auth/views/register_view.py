4bd14dc9da849bc45809536aa1ed9e6d04650d4a
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-10-29 16:56:12 -0400

    added alerts

--- core/auth/views/register_view.py
+++ core/auth/views/register_view.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.views import View
 from core.auth.forms.register_form import RegisterForm
 from django.contrib.auth import login
+from django.contrib import messages  # Import messages
 
 class RegisterView(View):
     form_class = RegisterForm
@@ -16,9 +17,9 @@ class RegisterView(View):
         if form.is_valid():
             user = form.save()
             login(request, user)
+            messages.success(request, 'Registration successful! You are now logged in.')
             return redirect('home')  # Replace 'home' with the name of your home page URL pattern
+        else:
+            # Show an error message if form is not valid
+            messages.error(request, 'Registration failed. Please check the form for errors.')
         return render(request, self.template_name, {'form': form})
-
-
-
-
