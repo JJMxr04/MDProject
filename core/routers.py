17228698a49bfeb4f93ba43ec07d4d748203bc64
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-10-29 20:34:12 -0400

    activate

--- core/routers.py
+++ core/routers.py
@@ -10,10 +10,10 @@ router = routers.SimpleRouter()
 # # ################### AUTH                       ###################### #
 # # ##################################################################### #
 #
-router.register(r'auth/register', RegisterViewSet, basename='auth-register')
-router.register(r'auth/login', LoginViewSet, basename='auth-login')
-router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
-router.register(r'auth/waitlist', WaitlistEntryViewSet , basename='auth-waitlist')
+# router.register(r'auth/register', RegisterViewSet, basename='auth-register')
+# router.register(r'auth/login', LoginViewSet, basename='auth-login')
+# router.register(r'auth/refresh', RefreshViewSet, basename='auth-refresh')
+# router.register(r'auth/waitlist', WaitlistEntryViewSet , basename='auth-waitlist')
 router.register(r'activate', ActivateUserViewSet, basename='activate')
 
 
@@ -21,22 +21,22 @@ router.register(r'activate', ActivateUserViewSet, basename='activate')
 # ################### USER                       ###################### #
 # ##################################################################### #
 
-router.register(r'user', UserViewSet, basename='user')
+# router.register(r'user', UserViewSet, basename='user')
 
 # ##################################################################### #
 # ################### TOURNAMENT                 ###################### #
 # ##################################################################### #
 
-router.register(r'tournaments', TournamentViewSet, basename='tournament')
+# router.register(r'tournaments', TournamentViewSet, basename='tournament')
 
-urlpatterns = [
-    *router.urls,
-    path('me/user/', UserMeViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update'}), name='user-me'),
-    path('', include(('core.event.routers', 'core'), namespace="core-api-event")),
-    path('activate/<str:token>/', ActivateUserViewSet.as_view({'get': 'retrieve'}), name='activate'),
-    path('', include(('core.match.routers', 'core'), namespace="core-api-match")),
-    path('', include(('core.game.routers', 'core'), namespace="core-api-game")),
-    path('admin/', include(('core.admin.routers', 'core'), namespace="core-api-core-admin")),
-    path('tournament/', include('core.tournament.routers')),  # Add this line
-    path('mail/', include('core.mail.urls')),
-]
+# urlpatterns = [
+#     *router.urls,
+#     path('me/user/', UserMeViewSet.as_view({'get': 'retrieve', 'patch': 'partial_update'}), name='user-me'),
+#     path('', include(('core.event.routers', 'core'), namespace="core-api-event")),
+#     path('activate/<str:token>/', ActivateUserViewSet.as_view({'get': 'retrieve'}), name='activate'),
+#     path('', include(('core.match.routers', 'core'), namespace="core-api-match")),
+#     path('', include(('core.game.routers', 'core'), namespace="core-api-game")),
+#     path('admin/', include(('core.admin.routers', 'core'), namespace="core-api-core-admin")),
+#     path('tournament/', include('core.tournament.routers')),  # Add this line
+#     path('mail/', include('core.mail.urls')),
+# ]
