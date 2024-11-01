8167a9ee19f6c0561d2e74b4e2a3f1339f1dff46
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-11-01 17:51:29 -0400

    need to add more to the forum stuff

--- core/event/urls.py
+++ core/event/urls.py
@@ -7,10 +7,11 @@ app_name = 'core-event'
 urlpatterns = [
     path('upcoming-events/', views.upcoming_events_list, name='upcoming-events'),
     path('upcoming-events/<str:event_id>/', views.upcoming_event_detail, name='upcoming-events-detail'),
-    # path('forums/', views.forum_list, name='forum_list'),
-    # path('forums/<int:forum_id>/threads/', views.thread_list, name='thread_list'),
-    # path('forums/<int:forum_id>/threads/create/', views.create_thread, name='create_thread'),
-    # path('threads/<int:thread_id>/posts/create/', views.create_post, name='create_post_thread'),
+    path('forums/', views.forum_list, name='forum_list'),
+    path('forums/<int:forum_id>/threads/', views.thread_list, name='thread_list'),
+    path('threads/<int:thread_id>/', views.thread_detail, name='thread_detail'),
+    path('forums/<int:forum_id>/threads/create/', views.create_thread, name='create_thread'),
+    path('threads/<int:thread_id>/posts/create/', views.create_post, name='create_post_thread'),
     path('<str:event_id>/posts/create/', views.create_post, name='create_post_event'),
     
 ]
--- core/event/views/forum.py
+++ core/event/views/forum.py
@@ -3,30 +3,38 @@ from django.contrib.auth.decorators import login_required
 from core.event.models import Forum, Thread, Post
 from core.event.forms import ThreadForm, PostForm
 from core.event.models import Event
+from django.shortcuts import render, get_object_or_404
 
-# def forum_list(request):
-#     forums = Forum.objects.all()
-#     return render(request, "forum/forum_list.html", {"forums": forums})
 
-# def thread_list(request, forum_id):
-#     forum = get_object_or_404(Forum, id=forum_id)
-#     threads = forum.threads.all()
-#     return render(request, "forum/thread_list.html", {"forum": forum, "threads": threads})
+def thread_detail(request, thread_id):
+    thread = get_object_or_404(Thread, id=thread_id)
+    # You may want to include related posts or other context here
+    return render(request, 'portal/event/thread_detail.html', {'thread': thread})
 
-# @login_required
-# def create_thread(request, forum_id):
-#     forum = get_object_or_404(Forum, id=forum_id)
-#     if request.method == "POST":
-#         form = ThreadForm(request.POST)
-#         if form.is_valid():
-#             thread = form.save(commit=False)
-#             thread.forum = forum
-#             thread.created_by = request.user
-#             thread.save()
-#             return redirect("thread_list", forum_id=forum.id)
-#     else:
-#         form = ThreadForm()
-#     return render(request, "forum/create_thread.html", {"form": form, "forum": forum})
+
+def forum_list(request):
+    forums = Forum.objects.all()
+    return render(request, "portal/event/forum_list.html", {"forums": forums})
+
+def thread_list(request, forum_id):
+    forum = get_object_or_404(Forum, id=forum_id)
+    threads = forum.threads.all()
+    return render(request, "portal/event/thread_list.html", {"forum": forum, "threads": threads})
+
+@login_required
+def create_thread(request, forum_id):
+    forum = get_object_or_404(Forum, id=forum_id)
+    if request.method == "POST":
+        form = ThreadForm(request.POST)
+        if form.is_valid():
+            thread = form.save(commit=False)
+            thread.forum = forum
+            thread.created_by = request.user
+            thread.save()
+            return redirect("core-portal:thread_list", forum_id=forum.id)
+    else:
+        form = ThreadForm()
+    return render(request, "portal/event/create_thread.html", {"form": form, "forum": forum})
 
 @login_required
 def create_post(request, thread_id=None, event_id=None):
@@ -41,9 +49,9 @@ def create_post(request, thread_id=None, event_id=None):
             post.created_by = request.user
             post.save()
             if thread:
-                return redirect("thread_detail", thread_id=thread.id)  # Adjust as necessary
+                return redirect("core-portal:thread_detail", thread_id=thread.id)  # Adjust as necessary
             if event:
                 return redirect("core-portal:upcoming-events-detail", event_id=event.id)  # Use the correct app name
     else:
         form = PostForm(initial={'thread': thread, 'event': event})
-    return render(request, "forum/create_post.html", {"form": form, "thread": thread, "event": event})
+    return render(request, "portal/event/create_post.html", {"form": form, "thread": thread, "event": event})
