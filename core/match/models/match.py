f0621fd14f5f61abe525d3533ca6183383a8dc00
JJMxr04 <maiaranagaming@gmail.com>
2024-11-03 17:09:55 -0500

    Update match.py

--- core/match/models/match.py
+++ core/match/models/match.py
@@ -81,6 +81,7 @@ class MatchManager(AbstractManager):
 
     def match_game_event_update(self, games, instance):
         for game in games:
+            print(game.match_id)
             match = self.get(id=game.match_id)
             game = Game.objects.game_event_update(game=game, instance=instance)
 
