5227525e2bf7c3963c5742c91fa68245066333b4
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-11-06 22:45:08 -0500

    Update settings.py

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -552,10 +552,10 @@ CELERY_BEAT_SCHEDULE = {
         'task': 'core.crons.tasks.sport_cron',
         'schedule': crontab(minute=0, hour=0),  # daily at midnight
     },
-    'event_cron': {
-        'task': 'core.crons.tasks.event_cron',
-        'schedule': crontab(minute=0, hour='*'),  # runs every hour
-    },
+    # 'event_cron': {
+    #     'task': 'core.crons.tasks.event_cron',
+    #     'schedule': crontab(minute=0, hour='*'),  # runs every hour
+    # },
     # 'print_cron_jobs': {
     #     'task': 'core.crons.tasks.print_cron_jobs',
     #     'schedule': crontab(minute='*'),  # every minute
