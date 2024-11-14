10f26919c73d91184dbb90f17bd1dead4d09335a
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-11-14 17:03:38 -0500

    Update player_in_match_required.py

--- core/match/decorators/player_in_match_required.py
+++ core/match/decorators/player_in_match_required.py
@@ -11,7 +11,7 @@ def player_in_match_required(view_func):
             return JsonResponse({'status': 'error', 'message': 'User not authenticated'}, status=401)
         
         match = get_object_or_404(Match, id=match_id)
-        if request.user != match.player_1 or request.user != match.player_2:
+        if request.user != match.player_1 and request.user != match.player_2:
             return JsonResponse({'status': 'error', 'message': 'Unauthorized user'}, status=403)
         return view_func(request, match_id, *args, **kwargs)
     return _wrapped_view
