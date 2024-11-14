b4cfa654863de80ce4e10d8e813b6af4bb8d287e
JJMxr04 <maiaranagaming@gmail.com>
2024-11-13 22:36:53 -0500

    Update myMatchDetail.py

--- core/match/views/myMatchDetail.py
+++ core/match/views/myMatchDetail.py
@@ -32,12 +32,13 @@ def my_match_detail_view(request, match_id):
     match = get_object_or_404(Match, id=match_id)
     is_player_in_match = request.user.id in [match.player_1.id, match.player_2.id]
     
-    # Filtering events if `match.end_date` is not None
+    # Filtering events to only include those that are 8 hours or greater from now in UTC and not completed
     if match.end_date:
         events = Event.objects.filter(
-            commence_time__gte=timezone.now() + timedelta(hours=8.25),
-            commence_time__lte=match.end_date
-        )
+            commence_time__gte=timezone.now() + timedelta(hours=8),  # Ensure this is in UTC
+            commence_time__lte=match.end_date,
+            completed=False  # Filter for events that are not completed
+        ).order_by('commence_time')  # Sort events by commence_time from earliest to latest
     else:
         events = Event.objects.none()  # Return an empty queryset if end_date is None
     
