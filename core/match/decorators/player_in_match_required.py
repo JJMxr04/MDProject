cf8d4bb8228b906b8cbd7dd7787ff19019e48f9e
JJMxr04 <87723660+JJMxr04@users.noreply.github.com>
2024-11-14 16:38:29 -0500

    Update player_in_match_required.py

--- core/match/decorators/player_in_match_required.py
+++ core/match/decorators/player_in_match_required.py
@@ -1,10 +1,14 @@
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from functools import wraps
+from core.match.models import Match
 
 def player_in_match_required(view_func):
     @wraps(view_func)
     def _wrapped_view(request, match_id, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return JsonResponse({'status': 'error', 'message': 'User not authenticated'}, status=401)
+        
         match = get_object_or_404(Match, id=match_id)
         if request.user != match.player_1 and request.user != match.player_2:
             return JsonResponse({'status': 'error', 'message': 'Unauthorized user'}, status=403)
