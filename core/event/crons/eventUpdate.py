24323fa293f0b02e9eefbecd7861e61186d42c46
JJMxr04 <maiaranagaming@gmail.com>
2024-11-24 11:12:45 -0500

    Update eventUpdate.py

--- core/event/crons/eventUpdate.py
+++ core/event/crons/eventUpdate.py
@@ -21,7 +21,9 @@ from core.event.models.bookmaker import Bookmaker
 from core.event.models.market import Market
 from core.event.models.outcome import Outcome
 
-from datetime import datetime, timezone, timedelta
+from django.utils import timezone
+
+from datetime import datetime, timedelta
 
 teamCron = TeamCron()
 
