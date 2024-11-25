f2b357215925dbee30af6ad53039189ed102978f
JJMxr04 <maiaranagaming@gmail.com>
2024-11-24 20:54:28 -0500

    Update eventUpdate.py

--- core/event/crons/eventUpdate.py
+++ core/event/crons/eventUpdate.py
@@ -330,7 +330,7 @@ class EventCron():
     
     def delete_outdated_events(self):
         # Get the current time in UTC
-        current_time = datetime.now(timezone.utc)
+        current_time = datetime.now()
         # Filter events that are not completed and have a commence_time that is 4 hours or more in the past
         outdated_events = Event.objects.filter(completed=False, commence_time__lt=current_time - timedelta(hours=4))
         
